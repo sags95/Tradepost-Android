14014e1a25ab0846343a773a53f559b5e5e40239
sharathsin <sharathjillela@gmail.com>
2015-06-01 17:54:19 -0400

    Added classes

---
+++ app/src/main/java/com/sinapp/sharathsind/tradepost/GCMReciever.java
@@ -0,0 +1,18 @@
+package com.sinapp.sharathsind.tradepost;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+public class GCMReciever extends WakefulBroadcastReceiver {
+    public GCMReciever() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
