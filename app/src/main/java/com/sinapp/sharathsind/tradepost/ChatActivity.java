a0550324987fd4cb145f8e3753842e787c1314cb
chianghomer <chianghomer@gmail.com>
2015-06-03 21:21:50 -0400

    Updates: -Chat Activity with attachment button (still working on it)

--- app/src/main/java/com/sinapp/sharathsind/tradepost/ChatActivity.java
+++ app/src/main/java/com/sinapp/sharathsind/tradepost/ChatActivity.java
@@ -1,6 +1,9 @@
 package com.sinapp.sharathsind.tradepost;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.etsy.android.grid.StaggeredGridView;
 
@@ -12,10 +15,40 @@ import Model.MarketPlaceDataAdapter;
  */
 public class ChatActivity extends NewToolBar {
 
+    ImageView attachBtn;
+    RelativeLayout sendBar, attachBar;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState, R.layout.activity_chat_room);
 
+        attachBtn = (ImageView)findViewById(R.id.attach_btn);
+        sendBar = (RelativeLayout)findViewById(R.id.send_bar);
+        attachBar = (RelativeLayout)findViewById(R.id.attach_bar);
+
+
+        RelativeLayout.LayoutParams attachBarparams = (RelativeLayout.LayoutParams)attachBar.getLayoutParams();
+
+
+
+        attachBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RelativeLayout.LayoutParams sendBarparams = (RelativeLayout.LayoutParams)sendBar.getLayoutParams();
+                RelativeLayout.LayoutParams attachBarparams = (RelativeLayout.LayoutParams)attachBar.getLayoutParams();
+                sendBarparams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, 0);
+                attachBarparams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM,1);
+                sendBar.setLayoutParams(sendBarparams);
+                attachBar.setLayoutParams(attachBarparams);
+            }
+        });
+
+
     }
+
+
+
+
 
 }
 
