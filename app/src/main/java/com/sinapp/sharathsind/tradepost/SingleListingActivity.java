789f1db743186b6d4a159aba7e9cbe1157f63a36
chianghomer <chianghomer@gmail.com>
2015-06-12 14:53:35 -0400

    Updates: -Image Slider in SingleListingActivity (Using ViewPager) -Dot Indicator

--- app/src/main/java/com/sinapp/sharathsind/tradepost/SingleListingActivity.java
+++ app/src/main/java/com/sinapp/sharathsind/tradepost/SingleListingActivity.java
@@ -1,20 +1,44 @@
 package com.sinapp.sharathsind.tradepost;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.CustomPagerAdapter;
 
 /**
  * Created by HenryChiang on 15-06-06.
  */
 public class SingleListingActivity extends Activity {
 
+    private CustomPagerAdapter mCustomPagerAdapter;
+    private final static int NUM_PAGES = 6;
+    private ViewPager mViewPager;
+    private List<ImageView> dots;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_listing_view);
+
+        //image slider viewer
+        mCustomPagerAdapter = new CustomPagerAdapter(this);
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mCustomPagerAdapter);
+
+        addDots();
+
     }
 
     @Override
@@ -37,5 +61,53 @@ public class SingleListingActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addDots() {
+        dots = new ArrayList<>();
+        LinearLayout dotsLayout = (LinearLayout)findViewById(R.id.dots);
+
+        for(int i = 0; i < NUM_PAGES; i++) {
+            ImageView dot = new ImageView(this);
+            if(i==0){
+                dot.setImageDrawable(getResources().getDrawable(R.drawable.pager_dot_selected));
+            }else {
+                dot.setImageDrawable(getResources().getDrawable(R.drawable.pager_dot_not_selected));
+            }
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                );
+            params.setMargins(3,0,3,0);
+
+            dotsLayout.addView(dot,params);
+
+           // dotsLayout.addView(dot);
+            dots.add(dot);
+        }
+
+        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                selectDot(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+    }
+
+    public void selectDot(int idx) {
+        Resources res = getResources();
+        for(int i = 0; i < NUM_PAGES; i++) {
+            int drawableId = (i==idx)?(R.drawable.pager_dot_selected):(R.drawable.pager_dot_not_selected);
+            Drawable drawable = res.getDrawable(drawableId);
+            dots.get(i).setImageDrawable(drawable);
+        }
     }
 }
