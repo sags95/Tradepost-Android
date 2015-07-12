5e82aff3389cb1a4d94e61af4a7e52a3537d1aca
chianghomer <chianghomer@gmail.com>
2015-07-12 10:15:14 -0400

    Update: -Notification

---
+++ app/src/main/java/Model/NotificationItem.java
@@ -0,0 +1,88 @@
+package Model;
+
+import android.graphics.Typeface;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.StyleSpan;
+
+/**
+ * Created by HenryChiang on 15-07-12.
+ */
+public class NotificationItem {
+
+    private String userPlaceholder, msg;
+    private int viewType;
+    private String[] defaultMsg = {
+            "has offered you an item.",
+            "has accepted your offer.",
+            "has declined your offer.",
+            "has delivered feedback.",
+            "Tradepost custom alert."
+    };
+
+    //viewType:
+    // 0 = trade offer.
+    // 1 = trade accepted.
+    // 2 = trade declined.
+    // 3 = feedback.
+    // 4 = tradepost custom alert.
+    // 5
+
+    public NotificationItem (String userPlaceholder, int viewType){
+        this.userPlaceholder = userPlaceholder;
+        this.viewType = viewType;
+
+    }
+
+    public String setMsg(){
+        String tempMsg="";
+        // create a bold StyleSpan to be used on the SpannableStringBuilder
+        switch (viewType){
+            case 0:
+                tempMsg = defaultMsg[0];
+                break;
+            case 1:
+                tempMsg = defaultMsg[1];
+                break;
+            case 2:
+                tempMsg = defaultMsg[2];
+                break;
+            case 3:
+                tempMsg = defaultMsg[3];
+                break;
+            case 4:
+                tempMsg = defaultMsg[4];
+                break;
+        }
+
+        SpannableStringBuilder sb = new SpannableStringBuilder(userPlaceholder + " " + tempMsg);
+        sb.setSpan(new StyleSpan(Typeface.ITALIC), 0, userPlaceholder.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE); // make first 4 characters Bold
+        this.msg = sb.toString();
+
+        return sb.toString();
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public int getViewType() {
+        return viewType;
+    }
+
+    public void setViewType(int viewType) {
+        this.viewType = viewType;
+    }
+
+    public String getUserPlaceholder() {
+        return userPlaceholder;
+    }
+
+    public void setUserPlaceholder(String userPlaceholder) {
+        this.userPlaceholder = userPlaceholder;
+    }
+
+    public String getDefaultMsg(int i) {
+        return defaultMsg[i];
+    }
+}
--- app/src/main/java/com/sinapp/sharathsind/tradepost/NotificationFragment.java
+++ app/src/main/java/com/sinapp/sharathsind/tradepost/NotificationFragment.java
@@ -3,10 +3,21 @@ package com.sinapp.sharathsind.tradepost;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.ChatPageAdapter;
+import Model.ChatPageItem;
+import Model.DividerItemDecoration;
+import Model.NotificationAdapter;
+import Model.NotificationItem;
 
 /**
  * Created by HenryChiang on 15-07-01.
@@ -14,6 +25,11 @@ import android.view.ViewGroup;
 public class NotificationFragment extends Fragment {
 
     private View rootView;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private NotificationAdapter mNotificationAdapter;
+    private Fragment chatFrag;
+    private ChatPageItem item;
 
 
     @Override
@@ -21,9 +37,34 @@ public class NotificationFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         rootView = inflater.inflate(R.layout.fragment_notification, container, false);
+        mRecyclerView = (RecyclerView)rootView.findViewById(R.id.noti_list);
+
+
+        final List<NotificationItem> notiItem = addItem("Longusername", 0);
+        mNotificationAdapter = new NotificationAdapter(notiItem);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getActivity(),DividerItemDecoration.VERTICAL_LIST));
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setAdapter(mNotificationAdapter);
+        applyLinearLayoutManager();
+
         return rootView;
+    }
+
+    public List<NotificationItem> addItem(String userName, int viewType) {
+        List<NotificationItem> items = new ArrayList<NotificationItem>();
+        items.add(new NotificationItem(userName,viewType));
+        items.add(new NotificationItem("User",1));
+        items.add(new NotificationItem("UserABC",2));
+        items.add(new NotificationItem("UserLong",3));
+        items.add(new NotificationItem("UserXYZ",4));
 
 
+
+        return items;
+    }
+    private void applyLinearLayoutManager(){
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
     }
 
     @Override
